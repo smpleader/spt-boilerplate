1990aac2cbf9febc2cee5628277ff2b6fa947376
joomaio.tinhcv <cvtinh2000@gmail.com>
2022-05-04 10:00:03 +0700

    prepare user and router

--- basic/php_modules/libraries/appPlg.php
+++ basic/php_modules/libraries/appPlg.php
@@ -11,6 +11,13 @@
 namespace App\libraries; 
 
 use SPT\App\JDIContainer\CMSApp; 
+use SPT\Router\SitemapEntity;
+use SPT\Router\Sitemap as Router;
+use SPT\Plugin\PluginEntity;
+use SPT\User\SPT\User;
+use SPT\User\Instance as UserIns;
+use SPT\User\SPT\UserEntity; 
+
 
 class appPlg extends CMSApp
 {
@@ -19,8 +26,32 @@ class appPlg extends CMSApp
         return 'App\\'. $extra;
     }
 
+    public function prepareRouter($config)
+    {
+        if($config->exists('endpoints'))
+        {
+            $sitePath = $config->exists('sitepath') ? $config->sitepath : '';
+
+            $sitemapEntity = new \stdClass();
+
+            if ($this->getContainer()->has('query'))
+            {
+                $sitemapEntity = new SitemapEntity($this->query);
+                $this->getContainer()->set('SitemapEntity', $sitemapEntity);
+            }
+            $router = new Router($sitemapEntity, $config, $this->request);
+            $this->getContainer()->share('router', $router, true);
+        }
+    }
+     
+
     public function prepareUser()
     {
-        // write your code here
-    }
+        $user = new UserIns( new User());
+        $user->init([
+            'session' => $this->session,
+            'entity' => new UserEntity($this->query)
+        ]);
+        $this->getContainer()->share('user', $user, true);
+    } 
 }
